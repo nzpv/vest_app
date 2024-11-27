5e22d46cc42a5e406c0b52b30622c9a495d5d92b
nz <enzo.calvet86@gmail.com>
2024-11-27 13:08:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+  mainWindow.loadURL('https://vest.lol');
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
